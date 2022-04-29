7d5f057ae6bbafd5e02274ffad29fe4f89cf2fe7
Chenlai Shi <33204827+shichenlai@users.noreply.github.com>
2022-04-29 10:19:50 -0500

    Add files via upload

--- day1/tsne_practice/tsne.py
+++ day1/tsne_practice/tsne.py
@@ -1,17 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def pca(X, no_dims=50):
     """
     Runs PCA on the nxd array X in order to reduce its dimensionality to
     no_dims dimensions.
-    
+
     Parameters
     ----------
     X : numpy.ndarray
-        data input array with dimension (n,d) 
+        data input array with dimension (n,d)
     no_dims : int
-        number of dimensions that PCA reduce to 
+        number of dimensions that PCA reduce to
 
     Returns
     -------
@@ -19,10 +20,11 @@ def pca(X, no_dims=50):
         low-dimensional representation of input X
     """
     n, d = X.shape
-    X = X - X.mean(axis=0)[None,:]
+    X = X - X.mean(axis=0)[None, :]
     _, M = np.linalg.eig(np.dot(X.T, X))
     Y = np.real(np.dot(X, M[:, :no_dims]))
     return Y
+
 
 if __name__ == "__main__":
     print("Run Y = tsne(X, no_dims, perplexity) to perform t-SNE on your dataset.")
